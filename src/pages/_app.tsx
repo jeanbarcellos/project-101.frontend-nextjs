b617b04eff11aba3e74fd235c2b16f9de3bd3131
Jean Barcellos <jeanbarcellos@hotmail.com>
2024-10-22 20:19:42 -0300

    Formatação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { AppProps } from 'next/app'
 import Layout from '../layout/Layout001'
-import "../styles/globals.css";
+import '../styles/globals.css'
 
- 
+
 // Next.js usa o Appcomponente para inicializar páginas
 // definir o layout global
 export default function App({ Component, pageProps }: AppProps) {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,26 @@
-import Link from "next/link";
+import Link from 'next/link'
 
 // home page
 export default function Home() {
   return (
     <>
-      <h1 className="mb-15">Hello, Next.js!</h1>
+      <h1 className='mb-15'>Hello, Next.js!</h1>
       <ul>
         <li>
-          <Link href="/">Home</Link>
+          <Link href='/'>Home</Link>
         </li>
         <li>
-          <Link href="/about">About Us</Link>
+          <Link href='/about'>About Us</Link>
         </li>
         <li>
-          <Link href="/post">Post</Link>
+          <Link href='/post'>Post</Link>
           <ul>
             <li>
-              <Link href="/post/1">Post 1</Link>
+              <Link href='/post/1'>Post 1</Link>
             </li>
           </ul>
         </li>
       </ul>
     </>
-  );
+  )
 }
