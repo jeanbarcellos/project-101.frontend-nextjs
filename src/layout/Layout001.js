d7e2d14f2af20da2351bc1b17397e15ff76ede6b
Jean Barcellos <jeanbarcellos@hotmail.com>
2024-10-22 21:57:57 -0300

    Ajustes no layout

--- src/layout/Layout001.js
+++ src/layout/Layout001.js
@@ -1,11 +1,51 @@
+import Link from 'next/link'
+
 export default function Layout001({ children }) {
+
+  const navigation = [
+    {
+      id: 'home',
+      text: 'Home',
+      url: '/'
+    },
+    {
+      id: 'about',
+      text: 'About Us',
+      url: '/about'
+    },
+    {
+      id: 'posts',
+      text: 'Posts',
+      url: '/posts'
+    },
+    {
+      id: 'contacts',
+      text: 'Contacts',
+      url: '/contacts'
+    }
+  ]
+
   return (
     <>
-      <div className='mb-10'>Layout 001 - Mavbar</div>
-      <hr />
-      <main>{children}</main>
-      <hr />
-      <div className='text-green-600'>Footer</div>
+      <header className='p-1 border-b-2 border-gray-300 text-center'>
+        Layout 001
+        <br />
+        Header
+      </header>
+
+      <nav className=''>
+        <ul>
+          {navigation.map(item => (
+            <li key={item.id}>
+              <Link href={item.url}>{item.text}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <main className='p-4'>{children}</main>
+
+      <footer className='p-1 border-y-2 border-gray-300 text-center'>Footer - Jean Barcellos @ 2024</footer>
     </>
   )
 }
