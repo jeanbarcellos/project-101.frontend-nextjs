3f275de0f07cd062d060aefd85d9842c1a6a999f
Jean Barcellos <jeanbarcellos@hotmail.com>
2025-01-25 15:30:23 -0300

    Add Material-UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
 import type { AppProps } from 'next/app'
 import Layout from '../layout/Layout001'
 import '../styles/globals.css'
+import { ThemeProvider } from '@mui/material'
+import themeConfig from '@/config/themeConfig'
 
 
 // Next.js usa o Appcomponente para inicializar páginas
 // definir o layout global
 export default function App({ Component, pageProps }: AppProps) {
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider theme={themeConfig}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   )
 }
